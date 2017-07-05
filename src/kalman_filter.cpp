ee2b3e3f9ffcc0aa854eb7f081a5ff14c5478854
Tim Aske <timaske@hotmail.com>
2017-07-05 18:59:58 -0300

    Completed project

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,22 +18,53 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_; // Under the impression that v (nu) is zero
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  // Evaluate h(x')
+  VectorXd hofx(3);
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // Pre-calculate px^2 + px^2 to avoid division by zero
+  float c1 = px*px + py*py;
+  if (fabs(c1) < 0.0000001) {
+    c1 = 0.0000001;
+  }
+
+  hofx << sqrt(c1), atan2(py, px), (px*vx + py*vy)/sqrt(c1);
+
+  // Update step - note that H = Hj is done outside of this class
+  VectorXd y = z - hofx;
+  // Ensure y(2) is in range (-pi, pi)
+  y[1] = atan2(sin(y[1]), cos(y[1])); // Taken from forums - originally tried looping additions/subtractions of 2*pi
+  // Continue with update
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
 }
